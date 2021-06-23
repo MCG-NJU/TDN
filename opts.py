dc2fa30bbb278a0acb553b05ee1b0c484268ad93
kanghaidong <kanghaidong@supremind.com>
2021-06-23 17:31:48 +0800

    Restructuring

--- opts.py
+++ opts.py
@@ -12,6 +12,7 @@ parser.add_argument('--train_list', type=str, default="")
 parser.add_argument('--val_list', type=str, default="")
 parser.add_argument('--root_path', type=str, default="")
 parser.add_argument('--store_name', type=str, default="")
+parser.add_argument('--decoded_type', type=str, default="decord",  choices=['decord','opencv'])
 # ========================= Model Configs ==========================
 parser.add_argument('--arch', type=str, default="resnet50")
 parser.add_argument('--num_segments', type=int, default=3)
